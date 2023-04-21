6a2e5abe61f6a74196b82ab037f87b63e16033a1
Udayanaik95 <naik.udaya25@gmail.com>
2023-04-21 14:11:05 +0530

    first commit

--- App/forms.py
+++ App/forms.py
@@ -10,6 +10,11 @@ class WebpageForm(forms.ModelForm):
     class Meta:
         model=Webpage
         fields='__all__'
+        #fields=['topic_name','name']
+        #exclude=['name']
+        labels={'topic_name':'Topic Name','name':'Name'}
+        #widgets={'name':forms.PasswordInput,'url':forms.Textarea}
+        help_texts={'topic_name':'Should not be integer','name':'Only Alphabet'}
 
 class AccessRecordForm(forms.ModelForm):
     class Meta:
